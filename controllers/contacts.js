a25a655bdb731d0964adf7a6df0c98ac274674f8
Vladyslav Ushakov <vlad.ushakov.977@gmail.com>
2023-07-05 17:06:38 +0300

    Add mongodb

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,14 +1,14 @@
-const contacts = require("../models/contacts");
+const Contact = require("../models/contact");
 const { HttpError, cntrlWrapper } = require("../helpers");
 
 const getAll = async (_, res) => {
-  const contactsList = await contacts.listContacts();
+  const contactsList = await Contact.find();
   res.json(contactsList);
 };
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const contact = await contacts.getContactById(contactId);
+  const contact = await Contact.findById(contactId);
 
   if (!contact) {
     throw HttpError(404, "Not found");
@@ -19,13 +19,13 @@ const getById = async (req, res) => {
 
 const add = async (req, res) => {
   const { body } = req;
-  const newContact = await contacts.addContact(body);
+  const newContact = await Contact.create(body);
   res.status(201).json(newContact);
 };
 
 const remove = async (req, res) => {
   const { contactId } = req.params;
-  const deletedContact = await contacts.removeContact(contactId);
+  const deletedContact = await Contact.findByIdAndRemove(contactId);
   if (!deletedContact) {
     throw HttpError(404, "Not found");
   }
@@ -40,7 +40,31 @@ const updateById = async (req, res) => {
     throw HttpError(400, "Missing fields");
   }
 
-  const updatedContact = await contacts.updateContact(contactId, body);
+  const updatedContact = await Contact.findByIdAndUpdate(contactId, body, {
+    new: true,
+  });
+
+  if (!updatedContact) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(updatedContact);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const { favorite } = req.body;
+
+  if (favorite === undefined) {
+    throw HttpError(400, "Missing field favorite");
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(
+    contactId,
+    { favorite },
+    {
+      new: true,
+    }
+  );
 
   if (!updatedContact) {
     throw HttpError(404, "Not found");
@@ -54,4 +78,5 @@ module.exports = {
   add: cntrlWrapper(add),
   remove: cntrlWrapper(remove),
   updateById: cntrlWrapper(updateById),
+  updateStatusContact: cntrlWrapper(updateStatusContact),
 };
